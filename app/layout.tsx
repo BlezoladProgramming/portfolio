6d39743ea9bf33c59f8cd1f4bb248606f702bae4
BlezoladProgramming <blezolad.1@gmail.com>
2025-12-24 19:59:21 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,16 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         {children}
+<a
+  href="https://www.facebook.com/BlezoladProgramming"
+  target="_blank"
+  className="fixed top-6 right-6 z-50 bg-blue-600 p-3 rounded-full shadow-lg hover:scale-110 transition"
+>
+  <svg className="w-5 h-5 fill-white" viewBox="0 0 24 24">
+    <path d="M22.675 0h-21.35C.597 0 0 .597 0 1.326v21.348C0 23.403.597 24 1.326 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.894-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.622h-3.12V24h6.116C23.403 24 24 23.403 24 22.674V1.326C24 .597 23.403 0 22.675 0z"/>
+  </svg>
+</a>
+
       </body>
     </html>
   );
